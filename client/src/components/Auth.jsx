dc28a61a9f1840223b86c2edc15fea76f476e8fe
Aremu <aremu.script@gmail.com>
2024-05-20 18:07:14 -0700

    pushing all

---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,173 @@
+import React, { useState } from 'react';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+
+import signinImage from '../assets/health.jpg';
+
+const cookies = new Cookies();
+
+const initialState = {
+    fullName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    phoneNumber: '',
+    // avatarURL: '',
+}
+
+const Auth = () => {
+    const [form, setForm] = useState(initialState);
+    const [isSignup, setIsSignup] = useState(true);
+    const [errorMessage, setErrorMessage] = useState('');
+
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const { username, password, phoneNumber } = form;
+
+        const URL = 'http://localhost:5000/auth';
+        // const URL = 'https://medical-pager.herokuapp.com/auth';
+
+        
+        try {
+            const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+                username, password, fullName: form.fullName, phoneNumber, 
+            });
+
+            cookies.set('token', token);
+            cookies.set('username', username);
+            cookies.set('fullName', fullName);
+            cookies.set('userId', userId);
+            
+            if(isSignup) {
+                cookies.set('phoneNumber', phoneNumber);
+                // cookies.set('avatarURL', avatarURL);
+                cookies.set('hashedPassword', hashedPassword);
+            }
+
+            window.location.reload();
+        } catch (error) {
+            if (error.response && error.response.data.message) {
+                setErrorMessage(error.response.data.message);
+            } else {
+                setErrorMessage('An unexpected error occurred. Please try again later.');
+            }
+        }
+    }
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+        setErrorMessage('');
+    }
+    
+    return (
+        <div className="auth__form-container">
+            <div className="auth__form-container_image">
+                <img src={signinImage} alt="sign in" />
+            </div>
+            <div className="auth__form-container_fields">
+                <div>
+                    <p className='auth__form-container_head'>Medify Chat</p>
+                </div>
+                <div className="auth__form-container_fields-content signupText">
+                    <p>{isSignup ? 
+                        (<>Start your journey towards better health and wellness today.<br /> <br />Sign up</>)
+                        : 
+                        (<>Hi, Welcome back to Medify Chat!<br /><br />Sign In</>)}
+                    </p>
+                    <form onSubmit={handleSubmit}>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="fullName">Full Name</label>
+                                <input 
+                                    name="fullName" 
+                                    type="text"
+                                    placeholder="Full Name"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="username">Username</label>
+                                <input 
+                                    name="username" 
+                                    type="text"
+                                    placeholder="Username"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="phoneNumber">Phone Number</label>
+                                <input 
+                                    name="phoneNumber" 
+                                    type="text"
+                                    placeholder="Phone Number"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        {/* {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="avatarURL">Avatar URL</label>
+                                <input 
+                                    name="avatarURL" 
+                                    type="text"
+                                    placeholder="Avatar URL"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )} */}
+                        <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="password">Password</label>
+                                <input 
+                                    name="password" 
+                                    type="password"
+                                    placeholder="Password"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <input 
+                                    name="confirmPassword" 
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            )}
+                        <div className="auth__form-container_fields-content_button">
+                            <button>{isSignup ? "Sign Up" : "Sign In"}</button>
+                        </div>
+                    </form>
+                    <div className="auth__form-container_fields-account">
+                        <p>
+                            {isSignup
+                             ? "Already have an account?" 
+                             : "Don't have an account?"
+                             }
+                             <span onClick={switchMode}>
+                             {isSignup ?  'Sign In' : 'Sign Up'}
+                             </span>
+                        </p>
+                    </div>
+                </div> 
+            </div>
+            
+        </div>
+    )
+}
+
+export default Auth
---
+++ client/src/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const Config = [
+  { apiKey: process.env.API_KEY }
+];
+
+module.exports = Config;
